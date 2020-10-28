018fdfb7c1490155af62d9a468ecc5e30907dddd
Wojtek Warmuz <wwarmuz@gmail.com>
2020-10-28 12:27:05 +0100

    add about section

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+export const about = ()=>{
+  const image = document.querySelector('.about__img');
+  let currentImage=0;
+  const images=["/images/wiosna.jpg","/images/zpilka.jpg","/images/hallo.jpg","/images/kominek.jpg"];
+  const imgChange=()=>{    
+image.setAttribute("src",images[currentImage]);
+if(currentImage<images.length-1){
+currentImage++}else{currentImage=0}
+gsap.fromTo(image, {opacity: 0}, {opacity:1, duration: 1});
+gsap.fromTo(image, {opacity: 1}, {opacity:0, duration: 1, delay:4});
+  }
+
+  setInterval(imgChange,5000);
+
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './sass/index.scss';
 import {bigAnime} from "./components/header.js";
+import {about} from "./components/about.js";
 
 
 window.addEventListener('DOMContentLoaded', bigAnime);
+window.addEventListener('DOMContentLoaded', about);
 
 
 
