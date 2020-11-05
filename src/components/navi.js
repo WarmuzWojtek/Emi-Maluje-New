531dc85694fb806b1267a00b2d6b5ae9ad6b015a
Wojtek Warmuz <wwarmuz@gmail.com>
2020-11-05 17:48:40 +0100

    navi animations refactor

--- src/components/navi.js
+++ src/components/navi.js
@@ -1,55 +1,16 @@
 export const navi=()=>{
-  const section1 = document.querySelector(".navi__nav-link-1");
-  const section2 = document.querySelector(".navi__nav-link-2");
-  const section3 = document.querySelector(".navi__nav-link-3");
-  const section4 = document.querySelector(".navi__nav-link-4");
-  const section5 = document.querySelector(".navi__nav-link-5");
-  const burger = document.querySelector('.header__burger');
-  section1.addEventListener('click',(e)=>{
+const burger = document.querySelector('.header__burger');
+const links = document.querySelectorAll(".navi__nav-link");
+links.forEach((link, index)=>{
+  link.addEventListener('click',(e)=>{
     e.preventDefault();
     burger.innerHTML='<i class="fas fa-bars"></i>';
-    gsap.to(window,{duration:0.1, scrollTo:{y:"#section1"}});
+    gsap.to(window,{duration:1, scrollTo:{y:"#section"+(index+1)}});
     gsap.to('.navi',{x:"-100vw",duration:0.1});
     gsap.to('.navi__listElement-cover',{x:0, duration:0.1,ease:Power0});
   })
-  section2.addEventListener('click',(e)=>{
-    e.preventDefault();
-    burger.innerHTML='<i class="fas fa-bars"></i>';
-    gsap.to('.navi',{x:"-100vw",duration:0.1});
-    gsap.to('.navi__listElement-cover',{x:0, duration:0.1,ease:Power0});
-    gsap.to(window,{duration:1, scrollTo:{y:"#section2"}});
-  })
-  section3.addEventListener('click',(e)=>{
-    e.preventDefault();
-    burger.innerHTML='<i class="fas fa-bars"></i>';
-    gsap.to(window,{duration:1, scrollTo:{y:"#section3"}});
-    gsap.to('.navi',{x:"-100vw",duration:0.1});
-    gsap.to('.navi__listElement-cover',{x:0, duration:0.1,ease:Power0});
-  })
-  section4.addEventListener('click',(e)=>{
-    e.preventDefault();
-    burger.innerHTML='<i class="fas fa-bars"></i>';
-    gsap.to(window,{duration:1, scrollTo:{y:"#section4"}});
-    gsap.to('.navi',{x:"-100vw",duration:0.1});
-    gsap.to('.navi__listElement-cover',{x:0, duration:0.1,ease:Power0});
-  })
-  section5.addEventListener('click',(e)=>{
-    e.preventDefault();
-    burger.innerHTML='<i class="fas fa-bars"></i>';
-    gsap.to(window,{duration:1, scrollTo:{y:"#section5"}});
-    gsap.to('.navi',{x:"-100vw",duration:0.1});
-    gsap.to('.navi__listElement-cover',{x:0, duration:0.1,ease:Power0});
-  })
-
-
-// const links = document.querySelectorAll(".navi__nav-link");
-// links.forEach((link, index)=>{
-//   link.addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     gsap.to(window,{duration:1, scrollTo:{y:"#section"+index+1}})
-//   })
   
-// })
+})
 }
 
 
